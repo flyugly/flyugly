cad18052d83a503fb2f76e3b708825c8994dab46
𝓯𝓵𝔂 𝓾𝓰𝓵𝔂 <flyugly.official@gmail.com>
2025-03-09 22:10:40 +0200

    Update product.py

--- product.py
+++ product.py
@@ -1,3 +1,3 @@
 import win32api, win32con, win32gui
 while True:
-    win32gui.MessabeBox(0, "Hey hey", "Titiwfsd", win32con.MB_OK)
+    win32gui.MessageBox(0, "Hey hey", "Titiwfsd", win32con.MB_OK)
